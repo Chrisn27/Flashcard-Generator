13892c5b6c669d7518813e7cf15eff2c0e52dddf
chrisn27 <chris_nguyen27@yahoo.com>
2017-05-30 16:17:35 -0700

    initial push

---
+++ BasicCard.js
@@ -0,0 +1,7 @@
+var BasicCard = function(front, back) {
+  // this.students will hold all of our student objects
+  this.front = front;
+  this.back = back;
+};
+
+module.exports = BasicCard
---
+++ ClozeCard.js
@@ -0,0 +1,1 @@
+var BasicCard = require("./.js");
