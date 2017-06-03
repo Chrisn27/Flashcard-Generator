bc3c9067c04a725a7a8a35f416685b0ca89ce2a5
chrisn27 <chris_nguyen27@yahoo.com>
2017-06-03 10:21:15 -0700

    inquirer hookup

--- BasicCard.js
+++ BasicCard.js
@@ -1,8 +1,25 @@
 var BasicCard = function(front, back) {
 
-    this.front = front;
-    this.back = back;
+    this.front = front.toLowerCase();
+    this.back = back.toLowerCase();
 
 };
+
+/*
+exports.basicQuestions = [
+	{
+		front: 'Which two pop princesses have a long running feud?',
+		back: 'Katy Perry and Taylor Swift'
+	},
+	{
+		front: 'Which one is taller?',
+		back: 'Taylor Swift'
+	},
+	{
+		front: 'What house is best in Game of Thrones?',
+		back: 'House Stark'
+	}
+]
+*/
 
 module.exports = BasicCard
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var BasicCard = require("./BasicCard.js");
 var ClozeCard = require("./ClozeCard.js");
+var inquirer = require('inquirer');
 
 /*
 var basicCardTest = new BasicCard("Who wrong the hit single, 'Shake it off'", "Taylor Swift");
@@ -17,3 +18,133 @@ console.log("-------------------------------------------------------------------
 console.log("---ClozeCard Error Checking---")
 var clozeCardError = new ClozeCard("Saying it's gonna be alright", "Cause the players gonna play, play, play, play, play");
 */
+
+var cardArr = [];
+var questionNum = 0;
+var correct = 0;
+
+var chooseType = function() {
+
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What type of cards do you want to make?",
+            choices: ["Basic", "Cloze"],
+            name: "cardtype"
+        }
+    ]).then(function(user) {
+
+        if (user.cardtype === "Basic") {
+            makeBasic();
+        } else {
+            makeCloze();
+        }
+    });
+};
+            
+var makeBasic = function () {
+
+    if (cardArr.length < 3) {
+        inquirer.prompt([
+            {
+                name: "front",
+                message: "Hint: "
+            }, 
+            {
+                name: "back",
+                message: "Answer: "
+            }
+        ]).then(function(answers) {
+
+            var bCard = new BasicCard (answers.front, answers.back)
+            cardArr.push(bCard);
+            makeBasic();
+
+        })
+
+            } else {
+                basicFlashGame();
+                //console.log(JSON.stringify(cardArr, null, 2));
+            }
+};
+        
+var makeCloze = function() {
+
+    if (cardArr.length < 3) {
+        inquirer.prompt([
+            {
+                name: "text",
+                message: "Full Text: "
+            }, 
+            {
+                name: "cloze",
+                message: "Cloze: "
+            }
+        ]).then(function(answers) {
+
+            var cCard = new ClozeCard (answers.text, answers.cloze)
+            cardArr.push(cCard);
+            makeCloze();
+
+        })
+
+      } else {
+        closeFlashGame();
+      }
+};
+
+var basicFlashGame = function() {
+
+    if (questionNum < cardArr.length) {
+        inquirer.prompt([
+            {
+			    type: 'input',
+			    message: "Hint: " + cardArr[questionNum].front + " : ",
+			    name: 'userGuess'
+            }
+        ]).then(function(answers) {
+            
+            if (answers.userGuess == cardArr[questionNum].back) {
+                correct += 1;
+                questionNum += 1;
+                basicFlashGame();
+            } else {
+                questionNum += 1;
+                basicFlashGame();
+            }
+
+        })
+
+     } else {
+        console.log("You got : " + correct + " correct");
+     }
+};
+
+var closeFlashGame = function() {
+
+    if (questionNum < cardArr.length) {
+        inquirer.prompt([
+            {
+			    type: 'input',
+			    message: "Hint: " + cardArr[questionNum].partial + " : ",
+			    name: 'userGuess'
+            }
+        ]).then(function(answers) {
+            
+            if (answers.userGuess == cardArr[questionNum].cloze) {
+                correct += 1;
+                questionNum += 1;
+                closeFlashGame();
+            } else {
+                questionNum += 1;
+                closeFlashGame();
+            }
+
+        })
+
+     } else {
+        console.log("You got : " + correct + " correct");
+     }
+};
+
+chooseType();
